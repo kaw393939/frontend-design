f745299f98b604891798add8e805bf2590607617
Sean Fisher <srtfisher@gmail.com>
2013-07-11 11:30:11 -0400

    Single page

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,9 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home Page' });
 };
+
+exports.single = function(req, res) {
+  res.render('single', {title: 'Single Page'});
+}
